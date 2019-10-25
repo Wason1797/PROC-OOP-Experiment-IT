f61eb0ce1c31022ea9bec21755c7c59af7577b8e
Cristina Díaz <cudiaza@gmail.com>
2019-10-25 17:00:04 -0500

    Resolucion

--- PROC-Back-End/app/main/functions.py
+++ PROC-Back-End/app/main/functions.py
@@ -8,7 +8,8 @@ def get_all(Model, Serializer):
 
 def calculate_order_price(order, ingredients):
     size_price = order.size.price
-    sum(ingredient.price for ingredient in ingredients)
+    ingredientes = sum(ingredient.price for ingredient in ingredients)
+    size_price+=ingredientes
     return round(size_price, 2)
 
 
